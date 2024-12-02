694a53add5008def737de91fdc7f38a09e57496d
bsse23057 <murtazasaleem2004@gmail.com>
2024-12-02 22:35:04 +0500

    created basic for Playlist Module

--- src/controllers/playlist.controller.js
+++ src/controllers/playlist.controller.js
@@ -1,12 +1,28 @@
 import { asyncHandler } from "../utils/Asynchandler";
+import { Playlist } from "../models/playlist.model";
+
+export const createPlaylist=asyncHandler(async(req,res)=>{
+    const {name,description}=req.body
+})
+export const updatePlaylist=asyncHandler(async(req,res)=>{
+    const {name,description}=req.body
+    const {id}=req.params
+})
+export const deletePlaylist=asyncHandler(async(req,res)=>{
+    const {id}=req.params
+})
+export const getPlaylistById=asyncHandler(async(req,res)=>{
+    const{id}=req.params 
+})
+export const getAllPlaylistById=asyncHandler(async(req,res)=>{
+    const {userId}=req.params
+
+})
 
 
-export const createPlaylist=asyncHandler(async(req,res)=>{})
-export const updatePlaylist=asyncHandler(async(req,res)=>{})
-export const deletePlaylist=asyncHandler(async(req,res)=>{})
-export const getPlaylistById=asyncHandler(async(req,res)=>{})
-export const getAllPlaylistById=asyncHandler(async(req,res)=>{})
-
-
-export const DeleteVedioFromPlaylist=asyncHandler(async(req,res)=>{})
-export const AddVedioToPlaylist=asyncHandler(async(req,res)=>{})
+export const DeleteVedioFromPlaylist=asyncHandler(async(req,res)=>{
+    const {vedioId,PlaylistId}=req.params
+})
+export const AddVedioToPlaylist=asyncHandler(async(req,res)=>{
+    const {vedioId,PlaylistId}=req.params
+})
