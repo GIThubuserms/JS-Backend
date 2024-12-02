5519d77f5e8513b6956c58f740d355926908d55f
bsse23057 <murtazasaleem2004@gmail.com>
2024-12-02 22:49:25 +0500

    worked on create Playlist

--- src/controllers/playlist.controller.js
+++ src/controllers/playlist.controller.js
@@ -1,8 +1,33 @@
-import { asyncHandler } from "../utils/Asynchandler";
-import { Playlist } from "../models/playlist.model";
+import { asyncHandler } from "../utils/Asynchandler.js";
+import { Playlist } from "../models/playlist.model.js";
+import { ApiError } from "../utils/ApiError.js";
+import {ApiResponse} from '../utils/Apiresponse.js'
 
 export const createPlaylist=asyncHandler(async(req,res)=>{
     const {name,description}=req.body
+    const user=req.user
+
+    console.log("Name : "+name);
+    console.log("Description : "+description);
+    console.log("UserId : "+user._id);
+
+    if(!(name && description)) throw new ApiError(400,"Name and Discription is Required !!")
+    
+    const newplaylist=await Playlist.create(
+        name,
+        description,
+        vedios=[],
+        owner=user._id
+    )
+    if(!newplaylist) throw new ApiError(502,"There was a error while creating a playlist !! ")
+
+    const findNewPlaylist=await Playlist.findById(newplaylist._id)   
+    if(!findNewPlaylist) throw new ApiError(402,"Playlist Not Fetching !!")
+ 
+    res
+    .status(200)
+    .json(new ApiResponse(200,newplaylist,"Playlist Created Successfully"))
+
 })
 export const updatePlaylist=asyncHandler(async(req,res)=>{
     const {name,description}=req.body
