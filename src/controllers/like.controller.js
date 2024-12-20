cf34a363728d8e2fa3a69afacf258e8dd0752871
bsse23057 <murtazasaleem2004@gmail.com>
2024-12-20 11:14:57 +0500

    Like Functionality done and tested

--- src/controllers/like.controller.js
+++ src/controllers/like.controller.js
@@ -4,112 +4,118 @@ import ApiResponse from "../utils/Apiresponse.js";
 import { ApiError } from "../utils/ApiError.js";
 import mongoose from "mongoose";
 
-export const ToggleVedioLike=asyncHandler(async(req,res)=>{
-    const {vedioId}=req.params
-    const user=req.user._id
-  
-    const Isvedioliked=await Like.findOne({
-        likedby:user,
-        vedio:vedioId  
+export const ToggleVedioLike = asyncHandler(async (req, res) => {
+    const { vedioId } = req.params
+    const user = req.user
+
+    const Isvedioliked = await Like.findOne({
+        likedby: user._id,
+        vedio: vedioId
     })
-    if(Isvedioliked){ /* unlike functionality*/
-      const VedioUnlike=await Like.findByIdAndDelete(Isvedioliked._id)
-      if(!VedioUnlike) throw new ApiError(502,"There was a error while unlike a vedio")
-      res.status(200).json(new ApiResponse(200,{},"Unliked"))
+    if (Isvedioliked) { /* unlike functionality*/
+        const VedioUnlike = await Like.findByIdAndDelete(Isvedioliked._id)
+        if (!VedioUnlike) throw new ApiError(502, "There was a error while unlike a vedio")
+        res.status(200).json(new ApiResponse(200, {}, "Unliked"))
     }
 
-    if(!Isvedioliked){ /*like functionality*/
-     const VedioLike=await Like.create({
-        likedby:user,
-        vedio:vedioId
-     })
-     if(!VedioLike) throw new ApiError(502,"There was a error while like a vedio")
-        res.status(200).json(new ApiResponse(200,{},"liked"))
+    if (!Isvedioliked) { /*like functionality*/
+        const VedioLike = await Like.create({
+            likedby: user._id,
+            vedio: vedioId
+        })
+        if (!VedioLike) throw new ApiError(502, "There was a error while like a vedio")
+        res.status(200).json(new ApiResponse(200, {}, "liked"))
     }
-    
+
 })
 
-export const ToggleCommentLike=asyncHandler(async(req,res)=>{
-    const {commentId}=req.params
-    const user=req.user._id
-  
-    const Iscommentliked=await Like.findOne({
-        likedby:user,
-        comment:commentId  
+export const ToggleCommentLike = asyncHandler(async (req, res) => {
+    const { commentId } = req.params
+    const user = req.user._id
+
+    const Iscommentliked = await Like.findOne({
+        likedby: user,
+        comment: commentId
     })
-    if(Iscommentliked){ /* unlike functionality*/
-      const commentUnlike=await Like.findByIdAndDelete(Iscommentliked._id)
-      if(!commentUnlike) throw new ApiError(502,"There was a error while unlike a comment")
-      res.status(200).json(new ApiResponse(200,{},"Unliked"))
+    if (Iscommentliked) { /* unlike functionality*/
+        const commentUnlike = await Like.findByIdAndDelete(Iscommentliked._id)
+        if (!commentUnlike) throw new ApiError(502, "There was a error while unlike a comment")
+        res.status(200).json(new ApiResponse(200, {}, "Unliked"))
     }
 
-    if(!Iscommentliked){ /*like functionality*/
-     const commentLike=await Like.create({
-        likedby:user,
-        comment:commentId
-     })
-     if(!commentLike) throw new ApiError(502,"There was a error while like a comment")
-        res.status(200).json(new ApiResponse(200,{},"liked"))
-    }
-})
-
-export const ToogleTweetLike=asyncHandler(async(req,res)=>{
-    const {tweetId}=req.params
-    const user=req.user._id
-  
-    const Istweetliked=await Like.findOne({
-        likedby:user,
-        tweet:tweetId  
-    })
-    if(Istweetliked){ /* unlike functionality*/
-      const tweetUnlike=await Like.findByIdAndDelete(Istweetliked._id)
-      if(!tweetUnlike) throw new ApiError(502,"There was a error while unlike a tweet")
-      res.status(200).json(new ApiResponse(200,{},"Unliked"))
-    }
-
-    if(!Istweetliked){ /*like functionality*/
-     const tweetLike=await Like.create({
-        likedby:user,
-        tweet:tweetId
-     })
-     if(!tweetLike) throw new ApiError(502,"There was a error while like a tweet")
-        res.status(200).json(new ApiResponse(200,{},"liked"))
+    if (!Iscommentliked) { /*like functionality*/
+        const commentLike = await Like.create({
+            likedby: user,
+            comment: commentId
+        })
+        if (!commentLike) throw new ApiError(502, "There was a error while like a comment")
+        res.status(200).json(new ApiResponse(200, {}, "liked"))
     }
 })
 
-export const GetAllLikedVedios=asyncHandler(async(req,res)=>{
-    const user=req.user._id
-    
-    const UserLikedVedios=await Like.aggregate([
+export const ToogleTweetLike = asyncHandler(async (req, res) => {
+    const { tweetId } = req.params
+    const user = req.user._id
+
+    const Istweetliked = await Like.findOne({
+        likedby: user,
+        tweet: tweetId
+    })
+    if (Istweetliked) { /* unlike functionality*/
+        const tweetUnlike = await Like.findByIdAndDelete(Istweetliked._id)
+        if (!tweetUnlike) throw new ApiError(502, "There was a error while unlike a tweet")
+        res.status(200).json(new ApiResponse(200, {}, "Unliked"))
+    }
+
+    if (!Istweetliked) { /*like functionality*/
+        const tweetLike = await Like.create({
+            likedby: user,
+            tweet: tweetId
+        })
+        if (!tweetLike) throw new ApiError(502, "There was a error while like a tweet")
+        res.status(200).json(new ApiResponse(200, {}, "liked"))
+    }
+})
+
+export const GetAllLikedVedios = asyncHandler(async (req, res) => {
+    const user = req.user._id
+
+    const UserLikedVedios = await Like.aggregate([
         {
-            $match:{
-                likedby:new mongoose.Types.ObjectId(user)
+            $match: {
+                likedby: new mongoose.Types.ObjectId(user),
+                vedio: { $exists: true, $ne: "", $ne: null }
             }
-        },{
-            $project:{
-                from:"vedios",
-                localefield:"vedio",
-                foriegnfield:"_id",
-                as:"vedio",
-                pipeline:[{
-                    $project:{
-                        Title:1,
-                        Discription:1,
-                        Vediofile:1,
-                        Thumbnail:1,
+        }, {
+            $project: {
+             vedio:1
+            }
+        },
+        {
+            $lookup: {
+                from: "vedios",
+                localField: "vedio",
+                foreignField: "_id",
+                as: "vedio",
+                pipeline: [{
+                    $project: {
+                        Title: 1,
+                        Discription: 1,
+                        Vediofile: 1,
+                        Thumbnail: 1,
                     }
                 }]
             }
         },
-        {
-            $project:{
-                vedio:1
-            }
-        }
+
     ])
 
-    if(UserLikedVedios.length<0) throw new ApiError(502,"Error while getting vedios")
+    if (UserLikedVedios.length < 0) throw new ApiError(502, "Error while getting vedios")
+    // const FilteredUserLikedVedios=UserLikedVedios.filter((vedio)=>vedio?.vedio[0])
+    // console.log(UserLikedVedios.length);
+    // console.log(FilteredUserLikedVedios.length);
+    // if(FilteredUserLikedVedios.length<0) throw new ApiError(502,"Error while getting vedios")
 
-        res.status(200).json(new ApiResponse(200,UserLikedVedios[0],"UserLike Vedio Fetched Successfully"))
+    res.status(200).json(new ApiResponse(200, UserLikedVedios, "UserLike Vedio Fetched Successfully"))
 })
 
--- src/routes/like.route.js
+++ src/routes/like.route.js
@@ -1,10 +1,13 @@
 import { Router } from "express";
 import { GetAllLikedVedios, ToggleCommentLike,ToggleVedioLike ,ToogleTweetLike } from "../controllers/like.controller.js";
+import { verifyuser } from "../middlewares/Auth.middleware.js";
 
 
 export const Likerouter=new Router()
 
+Likerouter.use(verifyuser)
+
 Likerouter.route('/toogle/v/:vedioId').post(ToggleVedioLike)
 Likerouter.route('/toogle/c/:commentId').post(ToggleCommentLike)
-Likerouter.route('/toogle/t/tweetId').post(ToogleTweetLike)
+Likerouter.route('/toogle/t/:tweetId').post(ToogleTweetLike)
 Likerouter.route('/vedios').get(GetAllLikedVedios)
